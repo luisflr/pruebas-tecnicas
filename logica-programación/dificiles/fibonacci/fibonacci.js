3501c30ca3059bd64c0d224034f7d8dc22b91082
luisflr <lfloresr@ulasalle.edu.pe>
2024-03-07 18:53:15 -0500

    logica-programacion/resolviendo fibonacci

--- logica-programación/dificiles/fibonacci/fibonacci.js
+++ logica-programación/dificiles/fibonacci/fibonacci.js
@@ -1,12 +1,10 @@
 const fibonacci = (size) => {
   let firstNumber = 0;
   let secondNumber = 1;
-  let temp = firstNumber;
   for( let i = 0; i < size; i++) {
     console.log(firstNumber)
     let sum = firstNumber + secondNumber
-    temp = secondNumber
-    firstNumber = temp
+    firstNumber = secondNumber
     secondNumber = sum
   }
 }

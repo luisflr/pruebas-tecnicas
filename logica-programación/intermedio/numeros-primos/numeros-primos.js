2de00bbc76bd15e20ebe6a1eba9556fdc418c3bd
luisflr <lfloresr@ulasalle.edu.pe>
2024-03-07 20:49:53 -0500

    logica-programacion/adding solution for problem of prime numbers

--- logica-programación/intermedio/numeros-primos/numeros-primos.js
+++ logica-programación/intermedio/numeros-primos/numeros-primos.js
@@ -1,9 +1,7 @@
 const primeValidation = (number) => {
   if (number < 2 ) return false
-  const divisors = [2,3,5,7]
-  if ( divisors.includes(number)) return true
-  for (let i = 0; i < divisors.length; i++) {
-    if(number % divisors[i] === 0) return false
+  for (let i = 2; i < number - 1; i++) {
+    if(number % i === 0) return false
   }
   return true
 }
